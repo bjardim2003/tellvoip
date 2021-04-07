03fa72ece06e9217c8105734e05db30881b0f7cf
Bruno Jardim <bruno2003@gmail.com>
2021-04-07 12:25:46 -0300

    Tellvoip

---
+++ preloadxxxx.js
@@ -0,0 +1,30 @@
+const {
+    remote,
+    ipcRenderer
+} = require("electron");
+
+//const $ = require('jquery')
+//const fs = require('fs')
+//const loki = require('lokijs')
+//const smpp = require('smpp');
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+    "api", {
+        send: (channel, data) => {
+            // whitelist channels
+            let validChannels = ["toMain"];
+            if (validChannels.includes(channel)) {
+                ipcRenderer.send(channel, data);
+            }
+        },
+        receive: (channel, func) => {
+            let validChannels = ["fromMain"];
+            if (validChannels.includes(channel)) {
+                // Deliberately strip event as it includes `sender` 
+                ipcRenderer.on(channel, (event, ...args) => func(...args));
+            }
+        }
+    }
+);
